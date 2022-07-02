e9d2863369ddea98331aad36881db3e1ed20a3c9
varm <v.racaud-minuzzi@orange.fr>
2022-07-02 18:35:14 +0200

    ✨ add a username for each client

--- ws_client.js
+++ ws_client.js
@@ -3,53 +3,111 @@ ws_config = {
     server_port: "8080", //by default
 };
 
+var wsSocket;
+
 function WSConnection() {
+    ws_config["server_address"] = document.getElementById("server-address").value;
+    ws_config["server_port"] = document.getElementById("server-port").value;
+
     var server_address = ws_config["server_address"];
     var server_port = ws_config["server_port"];
 
     var ws_string = "ws://" + server_address + ":" + server_port;
 
-    var wsSocket = new WebSocket(ws_string);
+    console.log("Connection to : " + ws_string);
 
-    //while the Room-modal is not open: animate a loading icon
+    try {
+        wsSocket = new WebSocket(ws_string);
+
+        wsSocket.addEventListener("open", (event) => {
+            document.getElementById("connection-error-label").textContent = "";
+            document.getElementById("WS-modal").style.display = "none";
+            document.getElementById("Room-modal").style.display = "block";
+        });
+
+        wsSocket.addEventListener("error", function(event) {
+            console.log("Error : ", event);
+            document.getElementById("connection-error-label").textContent =
+                "Connection Error: Server not reachable";
+        });
+
+        wsSocket.addEventListener("message", function(event) {
+            console.log("Message received from the server", event.data);
+            var dataJSON = JSON.parse(event.data);
+            var topic = dataJSON.topic;
+            var code = dataJSON.code;
+            console.log(topic);
+            if (topic == "BcMsg") {
+                var msgReceivedDiv = document.createElement("div");
+                msgReceivedDiv.className = "msgReceivedDiv";
+                msgReceivedDiv.textContent = dataJSON.msg;
+                document.getElementById("chatHistory").appendChild(msgReceivedDiv);
+            } else if (topic == "issue") {
+                var errorMsg;
+                switch (code) {
+                    case "rnau":
+                        errorMsg = "The room name is already used";
+                        break;
+                    case "wrongpwd":
+                        errorMsg = "Wrong Password";
+                        break;
+                    case "rde":
+                        errorMsg = "The room doesn't exist";
+                        break;
+                    case "uau":
+                        errorMsg = "The username is already used in the room";
+                        break;
+                    default:
+                        errorMsg = "Uncaught error";
+                }
+                document.getElementById("room-connection-error-label").textContent =
+                    errorMsg;
+            } else if (topic == "room") {
+                document.getElementById("room-connection-error-label").textContent = "";
+                document.getElementById("Room-modal").style.display = "none";
+            }
+        });
+        //disconnection event listener (in the server side): send that the user has disconnected to all other users in the chat room
+        //disconnection event listener: reset function call: WSsocket.close + display the WS-modal + message: "You've been disconnected from the server"
+    } catch {
+        document.getElementById("connection-error-label").textContent =
+            "Connection Error: Be sure that the credentials entered have a correct format";
+    }
 }
 
-wsSocket.addEventListener("open", (event) => {
-    //go to Room-modal
-});
-
-wsSocket.addEventListener("error", function(event) {
-    console.log("Error : ", event);
-    document.getElementById("connection-error-label").textContent =
-        "Connection Error";
-});
-
-wsSocket.addEventListener("message", function(event) {
-    console.log("Message received from the server", event.data);
-    var dataJSON = JSON.parse(event.data);
-    var topic = dataJSON.topic;
-    console.log(topic);
-    if (topic == "BcMsg") {
-        var msgReceivedDiv = document.createElement("div");
-        msgReceivedDiv.className = "msgReceivedDiv";
-        msgReceivedDiv.textContent = dataJSON.msg;
-        document.getElementById("chatHistory").appendChild(msgReceivedDiv);
-    }
-});
-
-//disconnection event listener
-
-function CreateRoom(roomName, roomPwd) {
+function CreateRoom() {
     if (wsSocket.readyState == WebSocket.OPEN) {
+        var roomName = document.getElementById("room-name").value;
+        var roomPwd = document.getElementById("room-pwd").value;
+        var username = document.getElementById("username").value;
         var topic = "CreateRoom";
-        var msg = { topic: topic, name: roomName, pwd: roomPwd };
+        var msg = {
+            topic: topic,
+            name: roomName,
+            pwd: roomPwd,
+            username: username,
+        };
         console.log(msg);
         wsSocket.send(JSON.stringify(msg));
     }
-    //else: return to the WS-modal and put in the connection-error-label that the client was disconnected
+    // TODO (in the disconnection listener): else: return to the WS-modal and put in the connection-error-label that the client was disconnected
 }
 
-//function JoinRoom()
+function JoinRoom() {
+    if (wsSocket.readyState == WebSocket.OPEN) {
+        var roomName = document.getElementById("room-name").value;
+        var roomPwd = document.getElementById("room-pwd").value;
+        var username = document.getElementById("username").value;
+        var topic = "JoinRoom";
+        var msg = {
+            topic: topic,
+            name: roomName,
+            pwd: roomPwd,
+            username: username,
+        };
+        wsSocket.send(JSON.stringify(msg));
+    }
+}
 
 function SendMessage() {
     var chatBox = document.getElementById("chatBox");
